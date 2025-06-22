3a80469e8f239a705f5a31cba42d47e4ab087676
PedroMion <pedromionbraga@gmail.com>
2025-06-22 09:38:50 -0300

    Starting to separate queues from servers to implement different disciplines

---
+++ qpy/queue_discipline.py
@@ -0,0 +1,35 @@
+from .job import Job
+from abc import ABC, abstractmethod
+from collections import deque
+from pydantic import validate_call
+
+
+class IQueue(ABC):
+    @abstractmethod
+    def insert(self):
+        pass
+    
+    @abstractmethod
+    def first_in_line(self):
+        pass
+
+class FirstComeFirstServed(IQueue):
+    def __init__(self):
+        self.queue = deque()
+    
+    def insert(self, job: Job):
+        self.queue.append(job)
+    
+    def first_in_line(self) -> Job:
+        return self.queue.popleft()
+
+class QueueDiscipline():
+    def __new__(cls, *args, **kwargs):
+        if cls is QueueDiscipline:
+            raise TypeError("Cannot instantiate this class directly.")
+        return super().__new__(cls, *args, **kwargs)
+
+    @staticmethod
+    @validate_call
+    def FCFS():
+        return FirstComeFirstServed()
