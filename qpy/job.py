23a56dfba1e26e2186d2aa2d6b87a7ce3ba19220
Pedro Mion <pedromionbraga@gmail.com>
2025-07-05 13:14:06 -0300

    Marking job class

--- qpy/job.py
+++ qpy/job.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 
 class Job:
-    def __init__(self, id, arrival_time, current_server, priority):
+    def __init__(self, id: int, arrival_time: float, current_server: int, priority: int):
         self.id = id
         self.arrival_time = arrival_time
         self.current_server = current_server
@@ -16,17 +16,17 @@ class Job:
         self.arrival_times_per_server[current_server].append(arrival_time)
         self.total_visits_per_server[current_server] += 1
 
-    def switch_servers_and_compute_state(self, time, server):
+    def switch_servers_and_compute_state(self, time: float, server: int):
         self.current_server = server
         self.arrival_time_at_current_server = time
         self.arrival_times_per_server[server].append(time)
         self.total_visits_per_server[server] += 1
 
-    def serve(self, service_started_time):
+    def serve(self, service_started_time: float):
         self.queue_times_per_server[self.current_server] += service_started_time - self.arrival_time_at_current_server
 
 
-    def reroute(self, completion_time, new_server = None):
+    def reroute(self, completion_time: float, new_server: int = None):
         self.total_time_per_server[self.current_server] += completion_time - self.arrival_time_at_current_server
 
         if new_server:
