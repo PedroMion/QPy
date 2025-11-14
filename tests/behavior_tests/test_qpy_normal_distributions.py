ea5b2a5fe1332da055bd5d6028c393602da359d9
PedroMion <pedromionbraga@gmail.com>
2025-11-14 14:28:51 -0300

    Add normal and uniform distributions

--- tests/behavior_tests/test_qpy_normal_distributions.py
+++ tests/behavior_tests/test_qpy_normal_distributions.py
@@ -8,47 +8,54 @@ DELTA_JOBS_PERCENTAGE = 0.15 # 15% margin of error
 TOTAL_SIMULATION_TIME = 20000
 WARMUP_TIME = 10000
 
-MU = 1
-LAMBDA = 0.5
+MU_SERVER = 4
+SIGMA_SERVER = 2
+MU_ARRIVAL = 8
+SIGMA_ARRIVAL = 2
+
+LAMBDA = 1/MU_ARRIVAL
+MU = 1/MU_SERVER
+
+EXPECTED_UTILIZATION = LAMBDA / MU
+EXPECTED_SERVICE_TIME = 1 / MU
 
 def _create_environment_with_servers(num_of_servers: int) -> Environment:
     env = Environment()
 
     for _ in range(num_of_servers):
-        env.add_server(service_distribution=Distribution.exponential(1/MU), queue_discipline=QueueDiscipline.fcfs())
+        env.add_server(service_distribution=Distribution.normal(mu=MU_SERVER, sigma=SIGMA_SERVER), queue_discipline=QueueDiscipline.fcfs())
 
-    env.add_entry_point(0, Distribution.exponential(1/LAMBDA))
+    env.add_entry_point(0, Distribution.normal(mu=MU_ARRIVAL, sigma=SIGMA_ARRIVAL))
 
     return env
 
-def test_open_network_MM1_exponential_FCFS():
+def test_open_network_MM1_normal_FCFS():
     env = _create_environment_with_servers(num_of_servers=1)
 
     ans = env.simulate(time_in_seconds=TOTAL_SIMULATION_TIME, warmup_time=WARMUP_TIME)
 
     expected_number_of_jobs = TOTAL_SIMULATION_TIME * LAMBDA
-    expected_time_in_system = (1/(MU - LAMBDA)) # Formula for M/M/1
-    expected_time_in_queue = expected_time_in_system - (1/MU) # Total time minus expected service time
+
+    ans.show_simulation_metrics()
 
     assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
-    assert value_within_range(expected_time_in_system, ans.environment_metrics.get_mean_time_in_system(), DELTA_JOBS_PERCENTAGE)
-    assert value_within_range(expected_time_in_queue, ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[0].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_SERVICE_TIME, ans.environment_metrics.get_mean_time_in_system() - ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
 
-def test_open_network_MM2_all_jobs_go_through_both_servers_exponential_FCFS():
+def test_open_network_MM2_all_jobs_go_through_both_servers_normal_FCFS():
     env = _create_environment_with_servers(num_of_servers=2)
     env.add_servers_connection(0, 1, 1)
 
     ans = env.simulate(time_in_seconds=TOTAL_SIMULATION_TIME, warmup_time=WARMUP_TIME)
 
     expected_number_of_jobs = TOTAL_SIMULATION_TIME * LAMBDA
-    expected_time_in_system = (1/(MU - LAMBDA)) * 2 # Two M/M/1's connected
-    expected_time_in_queue = expected_time_in_system - ((1/MU) * 2) # Two service times
 
     assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
     assert value_within_range(expected_number_of_jobs, ans.server_metrics[0].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
     assert value_within_range(expected_number_of_jobs, ans.server_metrics[1].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
-    assert value_within_range(expected_time_in_system, ans.environment_metrics.get_mean_time_in_system(), DELTA_JOBS_PERCENTAGE)
-    assert value_within_range(expected_time_in_queue, ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[0].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[1].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_SERVICE_TIME * 2, ans.environment_metrics.get_mean_time_in_system() - ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE) # Times two for two servers
 
 def test_open_network_MM3_all_jobs_go_through_server_0_and_half_chance_to_both_servers():
     env = _create_environment_with_servers(num_of_servers=3)
@@ -57,13 +64,13 @@ def test_open_network_MM3_all_jobs_go_through_server_0_and_half_chance_to_both_s
 
     ans = env.simulate(time_in_seconds=TOTAL_SIMULATION_TIME * 10, warmup_time=WARMUP_TIME)
 
-    expected_number_of_jobs = (TOTAL_SIMULATION_TIME * 10) * LAMBDA
-    expected_number_of_jobs_each_server = expected_number_of_jobs / 2 # 50% routing
-    expected_time_in_system = (1/(MU - LAMBDA)) + (1/(MU - (LAMBDA/2))) # Normal M/M/1 connected to other M/M/1 with half the jobs, given the division
-    expected_time_in_queue = expected_time_in_system - ((1/MU) * 2) # Both service times
+    expected_number_of_jobs = TOTAL_SIMULATION_TIME * 10 * LAMBDA
+    expected_number_of_jobs_each_server = expected_number_of_jobs / 2
 
     assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
     assert value_within_range(expected_number_of_jobs_each_server, ans.server_metrics[1].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
     assert value_within_range(expected_number_of_jobs_each_server, ans.server_metrics[2].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
-    assert value_within_range(expected_time_in_system, ans.environment_metrics.get_mean_time_in_system(), DELTA_JOBS_PERCENTAGE)
-    assert value_within_range(expected_time_in_queue, ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[0].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION / 2, ans.server_metrics[1].get_server_utilization(), DELTA_JOBS_PERCENTAGE) # Half because jobs split
+    assert value_within_range(EXPECTED_UTILIZATION / 2, ans.server_metrics[2].get_server_utilization(), DELTA_JOBS_PERCENTAGE) # Half because jobs split
+    assert value_within_range(EXPECTED_SERVICE_TIME * 2, ans.environment_metrics.get_mean_time_in_system() - ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
---
+++ tests/behavior_tests/test_qpy_uniform_distributions.py
@@ -0,0 +1,75 @@
+from qpy.distribution import Distribution
+from qpy.environment import Environment
+from qpy.queue_discipline import QueueDiscipline
+from utils import value_within_range
+
+
+DELTA_JOBS_PERCENTAGE = 0.10 # 10% margin of error
+TOTAL_SIMULATION_TIME = 20000
+WARMUP_TIME = 10000
+
+LOWER_BOUND_SERVER = 0.0001
+UPPER_BOUND_SERVER = 2.0001
+LOWER_BOUND_ARRIVAL = 0.5
+UPPER_BOUND_ARRIVAL = 2.5
+
+LAMBDA = 1/((LOWER_BOUND_ARRIVAL + UPPER_BOUND_ARRIVAL) / 2)
+MU = 1/((LOWER_BOUND_SERVER + UPPER_BOUND_SERVER) / 2)
+
+EXPECTED_UTILIZATION = LAMBDA / MU
+EXPECTED_SERVICE_TIME = 1 / MU
+
+def _create_environment_with_servers(num_of_servers: int) -> Environment:
+    env = Environment()
+
+    for _ in range(num_of_servers):
+        env.add_server(service_distribution=Distribution.uniform(LOWER_BOUND_SERVER, UPPER_BOUND_SERVER), queue_discipline=QueueDiscipline.fcfs())
+
+    env.add_entry_point(0, Distribution.uniform(LOWER_BOUND_ARRIVAL, UPPER_BOUND_ARRIVAL))
+
+    return env
+
+def test_open_network_MM1_uniform_FCFS():
+    env = _create_environment_with_servers(num_of_servers=1)
+
+    ans = env.simulate(time_in_seconds=TOTAL_SIMULATION_TIME, warmup_time=WARMUP_TIME)
+
+    expected_number_of_jobs = TOTAL_SIMULATION_TIME * LAMBDA
+
+    assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[0].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_SERVICE_TIME, ans.environment_metrics.get_mean_time_in_system() - ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
+
+def test_open_network_MM2_all_jobs_go_through_both_servers_uniform_FCFS():
+    env = _create_environment_with_servers(num_of_servers=2)
+    env.add_servers_connection(0, 1, 1)
+
+    ans = env.simulate(time_in_seconds=TOTAL_SIMULATION_TIME, warmup_time=WARMUP_TIME)
+
+    expected_number_of_jobs = TOTAL_SIMULATION_TIME * LAMBDA
+
+    assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(expected_number_of_jobs, ans.server_metrics[0].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(expected_number_of_jobs, ans.server_metrics[1].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[0].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[1].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_SERVICE_TIME * 2, ans.environment_metrics.get_mean_time_in_system() - ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE) # Times two for two servers
+
+def test_open_network_MM3_all_jobs_go_through_server_0_and_half_chance_to_both_servers():
+    env = _create_environment_with_servers(num_of_servers=3)
+    env.add_servers_connection(0, 1, 0.5)
+    env.add_servers_connection(0, 2, 0.5)
+
+    ans = env.simulate(time_in_seconds=TOTAL_SIMULATION_TIME * 10, warmup_time=WARMUP_TIME)
+
+    expected_number_of_jobs = TOTAL_SIMULATION_TIME * 10 * LAMBDA
+    expected_number_of_jobs_each_server = expected_number_of_jobs / 2
+
+    assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(expected_number_of_jobs_each_server, ans.server_metrics[1].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(expected_number_of_jobs_each_server, ans.server_metrics[2].get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(expected_number_of_jobs, ans.environment_metrics.get_number_of_processed_jobs(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION, ans.server_metrics[0].get_server_utilization(), DELTA_JOBS_PERCENTAGE)
+    assert value_within_range(EXPECTED_UTILIZATION / 2, ans.server_metrics[1].get_server_utilization(), DELTA_JOBS_PERCENTAGE) # Half because jobs split
+    assert value_within_range(EXPECTED_UTILIZATION / 2, ans.server_metrics[2].get_server_utilization(), DELTA_JOBS_PERCENTAGE) # Half because jobs split
+    assert value_within_range(EXPECTED_SERVICE_TIME * 2, ans.environment_metrics.get_mean_time_in_system() - ans.environment_metrics.get_mean_queue_time(), DELTA_JOBS_PERCENTAGE)
